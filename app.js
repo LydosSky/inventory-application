3b141ed72bf5f3c3af91356f14293f22a5c918aa
Lydos <lydossky@gmail.com>
2025-01-16 21:26:04 +0300

    feat: routes setup

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('node:path');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+app.use('/categories', categoryRouter);
+app.use('/products', productRouter);
+app.use('/inventory', inventoryRouter);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`Express app listenning on port ${PORT}!`);
 });

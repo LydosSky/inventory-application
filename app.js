424b9237d79b0cfb96c9a71b28677ad3f75d7103
Lydos <lydossky@gmail.com>
2025-01-17 22:36:15 +0300

    refactor: routes changed also database tables etc

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ const path = require('node:path');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const indexRouter = require('./routes/indexRouter');
+const categoryRouter = require('./routes/indexRouter');
+const productRouter = require('./routes/productRouter');
+const inventoryRouter = require('./routes/inventoryRouter');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  '/css',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')),
+);
+app.use(
+  '/css',
+  express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font/')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')),
+);
 
-app.use('/', indexRouter);
+app.use('/', stockRouter);
 app.use('/categories', categoryRouter);
 app.use('/products', productRouter);
-app.use('/inventory', inventoryRouter);
+app.use('/stores', storeRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
--- controllers/indexController.js
+++
@@ -1,7 +0,0 @@
-const database = require('../database/storeQueries');
-
-exports.getIndexPage = (req, res) =>
-  database.getAllStores().then((response) => {
-    console.log(response);
-    res.render('index', { title: 'Panel | Home', stores: response });
-  });

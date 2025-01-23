f150af099fb75dd3ae33f47e312b088941ce9c7c
Lydos <lydossky@gmail.com>
2025-01-23 22:08:03 +0300

    feat: add and update products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const productQueries = require('../database/productQueries');
+const categoryQueries = require('../database/categoryQueries');
 const _ = require('lodash');
 
 /**
@@ -6,12 +7,12 @@ const _ = require('lodash');
  * @param {Request} req
  * @param {Response} res
  * */
-exports.getAllProducts = (req, res) =>
-  productQueries
-    .getAllProducts()
-    .then((products) =>
-      res.render('products', { title: 'Products', products }),
-    );
+exports.getAllProducts = async (req, res) => {
+  const categories = await categoryQueries.getAllCategories();
+  const products = await productQueries.getAllProducts();
+
+  res.render('products', { title: 'Stock | Products', products, categories });
+};
 
 /**
  * Get Product by Id
@@ -31,7 +32,7 @@ exports.getProductById = (req, res) =>
 exports.addProduct = (req, res) =>
   productQueries
     .addProduct(({ name, description, price, category_id } = req.body))
-    .then(() => res.send('product added'));
+    .then(() => res.redirect('/products'));
 
 /**
  * Update Product
@@ -41,7 +42,7 @@ exports.addProduct = (req, res) =>
 exports.updateProduct = (req, res) =>
   productQueries
     .updateProduct(({ id, name, description, price, category_id } = req.body))
-    .then(() => res.send('product updated'));
+    .then(() => res.redirect('/products'));
 
 /**
  * Delete Product
